1fad5ecb51507bbef9473176036c8c6aebbc1d0a
DavidMayorgaS08 <davidmayorga@colegioguanenta.edu.co>
2022-07-12 09:00:30 -0500

    curso php

---
+++ variables.php
@@ -0,0 +1,22 @@
+<?php
+// Comentario
+
+/* comentario 
+*/
+
+echo "Hola";
+echo "<br/>";
+
+//Variable
+$nombre = "David";
+$apellido = "Mayorga";
+$edad = 16;
+
+//Concatenacion
+$imprimir = $nombre ." ". $apellido ." ". $edad;
+echo $imprimir;
+
+//Ciclo for
+
+
+?>
